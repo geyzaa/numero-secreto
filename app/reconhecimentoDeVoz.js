0c47d9c7466691270dd33eef70f7ef91605fe72e
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-02-28 18:15:06 -0300

    definindo a voz com Web Speech

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,23 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || 
+window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+//variavel + evento (pegar result, função)
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    chute = e.results [0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
