7de238621dc61b9da83343478facb5ac10529030
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-02-28 19:41:04 -0300

    botão - reinicia jogo

--- app/validacao.js
+++ app/validacao.js
@@ -18,6 +18,8 @@ function verificaSeChutePossuiValorValido(chute) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar"> Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML +=`
@@ -38,3 +40,10 @@ function chuteForInvalido(numero) {
 function numeroMaiorOuMenorQueOPermitido(numero) {
     return numero > maiorValor  || numero < menorValor 
 }
+
+//reiniciar jogo 
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
